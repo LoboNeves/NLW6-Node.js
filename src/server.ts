7e1108c059fae4008d61cdc53acae1021d02a586
DanielNeves <dbenditoneves@gmail.com>
2021-06-22 17:46:57 -0300

    Ensinamentos da aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,36 @@
 import express from "express";
+import "reflect-metadata";
+import "./database";
+import { router } from "./routes";
 
 // @types/express
 const app = express();
+
+app.use(express.json());
+
+app.use(router); // Inserir as rotas dentro do express, para que todas as rotas façam parte do projeto
+
+// http://localhost:3000
+app.listen(3000, () => console.log("Server is running"));
+
+/*
+ * GET => Buscar uma informação
+ * POST => Inserir(Criar) uma informação
+ * PUT => Alterar uma informação
+ * DELETE => Remover uma informação
+ * PATCH => Alterar uma informação específica
+
+
+
+ * Tipos de parâmetros
+ * Routes params => http://localhost:3000/products/123154
+ * Query Params => http://localhost:3000/products?name=keyboard&description=goodkeyboard&
+ * Body params => {
+ *  "name": "Keyboard"
+ *  "description": "good keyboard"
+ * }
+
+
 
 app.get("/test", (request, response) => {
     //request => entrando
@@ -12,6 +41,4 @@ app.get("/test", (request, response) => {
 app.post("/test-post", (request, response) => {
     return response.send("Test POST")
 })
-
-// http://localhost:3000
-app.listen(3000, () => console.log("Server is running"));
+*/
